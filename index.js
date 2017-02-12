24b2a40ac0795ce7d20c68d45eb1dfc3b2613235
System Administrator <root@apple-mbp-7.attlocal.net>
2017-02-11 22:04:15 -0600

    .

--- index.js
+++ index.js
@@ -15,13 +15,12 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-	res.send('1068239605')
+	res.send('Hello, I am a chat bot.')
 })
-
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req. query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong token')
